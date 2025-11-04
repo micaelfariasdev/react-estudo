2de6ec3b4ccd63230f3e2f76c0a2f8f716f42c97
Micael Farias <Micaelfarias.dev@gmail.com>
2025-11-04 13:01:24 -0300

    add page /odd

--- te.js
+++
@@ -1,8 +0,0 @@
-async function buscarImagem(termo) {
-  const r = await fetch(`http://localhost:3001/buscar-imagem?q=${encodeURIComponent(termo)}`);
-  const j = await r.json();
-  return j.img;
-}
-
-const img = await buscarImagem("vasco");
-console.log(img);
